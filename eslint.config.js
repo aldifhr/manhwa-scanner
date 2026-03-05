242501e047234747ccc131c9552f78ca0c363b5a
aldifhrent <aldifahriziilmawan@gmail.com>
2026-03-06 03:16:50 +0700

    fix: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,35 @@
-import js from '@eslint/js';
-import globals from 'globals';
+import js from "@eslint/js";
+import globals from "globals";
 
 export default [
   // Global ignores (ganti .eslintignore)
-  { ignores: [
-    'node_modules/',
-    'dist/',
-    '.vercel/',
-    '*.test.js',
-    'test-*.js',
-    'bulk-add.js',
-    'flush.js',
-    'migrate.js',
-    'cron.js'
-  ] },
-  
+  {
+    ignores: [
+      "node_modules/",
+      "dist/",
+      ".vercel/",
+      "*.test.js",
+      "test-*.js",
+      "bulk-add.js",
+      "flush.js",
+      "migrate.js",
+      "cron.js",
+    ],
+  },
+
   // Main config
   js.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: globals.node
+      sourceType: "module",
+      globals: globals.node,
     },
     rules: {
-        'no-console': 'off' ,
-      'no-unused-vars': 'error',
-      'eqeqeq': 'error',
-    }
-  }
+      "no-console": "off",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+      eqeqeq: "error",
+    },
+  },
 ];
